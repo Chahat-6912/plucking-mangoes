be952e922a80f998080b3fa1e003af9eed000a55
Chahat-6912 <74601447+Chahat-6912@users.noreply.github.com>
2021-02-26 20:06:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1100,100,30);
+	mango3=new mango(1100,100,30);
+	mango4=new mango(1100,100,30);
+	mango5=new mango(1100,100,30);
+	mango6=new mango(1100,100,30);
+	mango7=new mango(1100,100,30);
+	mango8=new mango(1100,100,30);
+	mango9=new mango(1100,100,30);
+	mango10=new mango(1100,100,30);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	stone1 = new stone(100,100)
+	sling = new Slingshot(stone.body,{x:200,y:100})
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+
+  groundObject.display();
+}
